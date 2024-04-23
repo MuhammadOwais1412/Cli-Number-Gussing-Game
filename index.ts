fd088f75175394d82a66f955255b845caf0003aa
MuhammadOwais1412 <habibci2005@ymail.com>
2024-04-23 10:05:39 +0500

    Adding a Colors in Project

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 #! /usr/bin/env node
+import chalk from "chalk";
 import inquirer from "inquirer";
 
-console.log("\n\tWelcome to Code With Owais - CLI Number Guessing Game\n");
+console.log(chalk.bold.rgb(179, 160, 255)("\n\tWelcome to Code With Owais - CLI Number Guessing Game\n"));
 
 const randomNumber = Math.floor(Math.random() * 5 + 1);
 
@@ -14,9 +15,9 @@ const answer = await inquirer.prompt([
 ]);
 
 if (answer.userGuessedNumber === randomNumber) {
-  console.log("Congratulation! You guessed a correct number");
+  console.log(chalk.greenBright("\nCongratulation! You guessed a correct number"));
 } else if (answer.userGuessedNumber > 5) {
-  console.log("Please enter a number between 1 to 5");
+  console.log("\nPlease enter a number between 1 to 5");
 } else {
-  console.log("Sorry! You guessed a wrong number");
+  console.log(chalk.redBright("\nSorry! You guessed a wrong number"));
 };
